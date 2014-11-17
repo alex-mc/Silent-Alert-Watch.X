2cf0bcfe4e1ac160604a99f2a84dcce96ed9517f
alex-mc <mcmaster2010@gmail.com>
2014-11-17 11:02:42 -0800

    initial commit w/ pseudocode

---
+++ saw_classifier_main.c
@@ -0,0 +1,27 @@
+/* 
+ * File:   saw_classifier_main.c
+ * Author: alex-mc
+ *
+ * Created on November 17, 2014, 10:59 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main() {
+    // initialize timer, ISR, and I/O pins
+    while(1) {
+        // run FFT
+        // classify as something or nothing
+        // if something set vibrate pin
+        // classify specifically
+        // print classification(s) to LCD
+        while(1) {
+            // wait for timer ISR
+        }
+    }
+}
+
