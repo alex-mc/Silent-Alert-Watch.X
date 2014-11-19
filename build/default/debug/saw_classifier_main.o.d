40d1640b734fadfe8e97a3f724d37afc1eaa8a50
osakary <osakary@gmail.com>
2014-11-19 15:05:55 -0800

    Revert "Revert "stuff""
    This reverts commit 7bd3a7b9a4e9dce7707f315175a23f39bb95c8d2.

---
+++ build/default/debug/saw_classifier_main.o.d
@@ -0,0 +1,5 @@
+build/default/debug/saw_classifier_main.o:  \
+	C:/Users/alexmc/Documents/GitHub/Silent-Alert-Watch.X/saw_classifier_main.c  \
+	C:/Users/alexmc/Documents/GitHub/Silent-Alert-Watch.X/fft.h  \
+	C:/Users/alexmc/Documents/GitHub/Silent-Alert-Watch.X/classifier.h  \
+
--- fft.h
+++ fft.h
@@ -1,9 +1,26 @@
-/* 
- * File:   fft.h
- * Author: alexmc
- *
- * Created on November 19, 2014, 10:34 AM
- */
+/************************************************************************
+	fft.h
+
+    FFT Audio Analysis
+ * Code borrowed from Simon Inns (simon.inns@gmail.com)
+ * Significant changes made by Alex McMaster and Hieu Duong
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+	Email: mcmaster2010@gmail.com
+
+************************************************************************/
 
 #ifndef FFT_H
 #define	FFT_H
